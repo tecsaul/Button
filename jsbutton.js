d4e3ab6777ca7821dfde01d8ffada5acecc5a44d
saul <saul.nunez18@tectijuana.edu.mx>
2023-05-14 22:27:23 -0700

    Se agregaron js y se cambiarion textos

---
+++ jsbutton.js
@@ -0,0 +1,15 @@
+cube1.addEventListener('click', () => {
+  window.location.href = '/cube.html';
+});
+
+cube2.addEventListener('click', () => {
+  window.location.href = '/cube.html';
+});
+
+cube3.addEventListener('click', () => {
+  window.location.href = '/cube.html';
+});
+
+cube4.addEventListener('click', () => {
+  window.location.href = '/cube.html';
+});
--- jscube.js
+++ jscube.js
@@ -1,10 +1,15 @@
-var cube = document.querySelector('.cube');
-var radioGroup = document.querySelector('.radio-group');
-var currentClass = '';
+let cube = document.querySelector('.cube');
+let regresar = document.querySelector('#regresar');
+let radioGroup = document.querySelector('.radio-group');
+let currentClass = '';
+
+regresar.addEventListener('click', () => {
+  window.location.href = '/button.html';
+});
 
 function changeSide() {
-  var checkedRadio = radioGroup.querySelector(':checked');
-  var showClass = 'show-' + checkedRadio.value;
+  let checkedRadio = radioGroup.querySelector(':checked');
+  let showClass = 'show-' + checkedRadio.value;
   if ( currentClass ) {
     cube.classList.remove( currentClass );
   }
